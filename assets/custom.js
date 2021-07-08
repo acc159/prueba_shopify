c178730028ab9114932727c8c11d0eca5d2a4fa9
Adrian Caparros <caparrosmail97@gmail.com>
2021-07-08 12:26:15 +0200

    Update custom.js

--- assets/custom.js
+++ assets/custom.js
@@ -1,10 +1,5 @@
 function init(){
-    let top = document.querySelector('.el_mejor_producto');
-    if(top){
-        alert('Tenemos el producto top');
-    }else{
-        alert('No tenemos producto top')
-    }
+    console.log('Imprimir por consola');
 }
 
 
